34f297dfecc6dbc62d0a96a517374af19d95fc20
sethvincent <sethvincent@gmail.com>
2013-06-16 06:46:42 -0700

    refactoring

--- app.js
+++ app.js
@@ -1,46 +1,106 @@
 var Game = function(canvas, width, height){
-  var c = new Coquette(this, canvas, width, height, '#fff');
+  this.coquette = new Coquette(this, canvas, width, height, '#fff');
+};
 
-  c.entities.create(Blob, { 
-    size: { x: 100, y: 100 },
-    pos: { x: randPosX(), y: randPosY() },
-    speed: 10,
-    color: '#efefef',
-    update: function(){
-      if (c.inputter.state(c.inputter.UP_ARROW)) {
+Game.prototype.init = function(){
+    this.coquette.entities.create(Blob, { 
+      size: { x: 100, y: 100 },
+      pos: { x: randPosX(), y: randPosY() },
+      boundingBox: this.coquette.collider.RECTANGLE,
+      speed: 10,
+      color: '#efefef'   
+  });
+};
+
+/*
+  function(blob){
+    window.addEventListener('click', function(e){
+      c.entities.create(BlobSnot, {
+        size: { x: 10, y: 10 },
+        pos: { 
+          x: blob.pos.x + (blob.size.x / 2) - 5, 
+          y: blob.pos.y + (blob.size.y / 2) - 5
+        },
+        boundingBox: c.collider.RECTANGLE,
+        speed: 10,
+        color: '#ff0000'
+      });
+    });
+  });
+*/
+
+var Blob = function(game, settings){
+  for (var i in settings){
+    this[i] = settings[i];
+    console.log(i)
+  }
+
+  this.game = game;
+  var c = this.game.coquette;
+
+  this.size = { x: settings.size.x, y: settings.size.y };
+
+  this.draw = function(context){
+    context.fillStyle = settings.color;
+    context.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+  };
+
+  this.collision = function(other, type){
+
+  };
+
+  this.uncollision = function(other, type){
+
+  };
+
+  this.update = function(){
+      this.handleKeyboard();
+      this.handleBoundaries();
+    },
+
+  this.handleKeyboard = function(){
+      if (c.inputter.state(c.inputter.W)) {
         this.pos.y -= this.speed;
       }
-      if (c.inputter.state(c.inputter.DOWN_ARROW)) {
+
+      if (c.inputter.state(c.inputter.S)) {
         this.pos.y += this.speed;
       }
-      if (c.inputter.state(c.inputter.LEFT_ARROW)) {
+
+      if (c.inputter.state(c.inputter.A)) {
         this.pos.x -= this.speed;
       }
-      if (c.inputter.state(c.inputter.RIGHT_ARROW)) {
+
+      if (c.inputter.state(c.inputter.D)) {
         this.pos.x += this.speed;
       }
+
       if (c.inputter.state(c.inputter.SHIFT)){
         this.pos.x = randPosX();
         this.pos.y = randPosY();
       }
+    },
 
+    this.handleBoundaries = function(){
       if (this.pos.y <= 0){
         this.pos.y = 0;
       }
+
       if (this.pos.y >= c.renderer.height - this.size.y){
         this.pos.y = c.renderer.height - this.size.y;
       }
+
       if (this.pos.x <= 0){
         this.pos.x = 0;
       }
+
       if (this.pos.x >= c.renderer.width - this.size.x){
         this.pos.x = c.renderer.width - this.size.x;
-      }
+      } 
     }
-  });
-}
+};
 
-var Blob = function(_, settings){
+var BlobSnot = function(_, settings){
   for (var i in settings){
     this[i] = settings[i];
   }
@@ -53,7 +113,6 @@ var Blob = function(_, settings){
   };
 
   this.collision = function(other, type){
-
   };
 
   this.uncollision = function(other, type){
@@ -70,5 +129,6 @@ function randPosY(){
 }
 
 window.addEventListener('load', function(){
-  new Game('game', document.width, document.height);
+  var game = new Game('game', document.width, document.height);
+  game.init();
 });
